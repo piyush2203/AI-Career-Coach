46279a6e3050ed2a8e1d46a4cb8d9a172f387919
Piyush Gupta <piyushgupta21506@gmail.com>
2025-03-23 00:54:29 +0530

    final 6

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,13 +1,10 @@
-import { inngest } from "@/lib/inngest/client";
-// import { helloWorld } from "@/lib/inngest/function";
 import { serve } from "inngest/next";
 
+import { inngest } from "@/lib/inngest/client";
+import { generateIndustryInsights } from "@/lib/inngest/function";
+// import { generateIndustryInsights } from "@/lib/inngest/function";
 
-// Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  functions: [
-    /* your functions will be passed here later! */
-    
-  ],
+  functions: [generateIndustryInsights],
 });
